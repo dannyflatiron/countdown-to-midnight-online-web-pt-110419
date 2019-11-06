5162623313b4f0c3f40016fe0e54c6f723d338c0
Daniel Reina <dannyflatiron@gmail.com>
2019-11-06 22:44:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
-def countdown(integer)
-  integer = 10
-  while integer > 0
-    puts "#{integer} SECOND(S)!"
-    integer -=
-  end
-  "HAPPY NEW YEAR!"
-
-end
+# def countdown(integer)
+#   integer = 10
+#   while integer > 0
+#     puts "#{integer} SECOND(S)!"
+#     integer -=
+#   end
+#   "HAPPY NEW YEAR!"
+#
+# end
